6f769cf11dc3efbf19a53db1ec0e57018eacaaba
TITI3048 <164487053+TITI3048@users.noreply.github.com>
2024-08-14 10:22:39 +0200

    jungle.html jungle.css jungle.js

--- jungle/jungle.js
+++ jungle/jungle.js
@@ -1,58 +1,20 @@
-let items = document.querySelectorAll(".slider .item");
-let active = 3;
-function loadShow() {
-  items[active].style.transform = `none`;
-  items[active].style.zIndex = 1;
-  items[active].style.filter = "none";
-  items[active].style.opacity = 1;
-  // show after
-  let stt = 0;
-  for (var i = active + 1; i < items.length; i++) {
-    stt++;
-    items[i].style.transform = `translateX(${120 * stt}px) scale(${
-      1 - 0.2 * stt
-    }) perspective(16px) rotateY(-1deg)`;
-    items[i].style.zIndex = -stt;
-    items[i].style.filter = "blur(5px)";
-    items[i].style.opacity = stt > 2 ? 0 : 0.6;
-  }
-  stt = 0;
-  for (var i = active - 1; i >= 0; i--) {
-    stt++;
-    items[i].style.transform = `translateX(${-120 * stt}px) scale(${
-      1 - 0.2 * stt
-    }) perspective(16px) rotateY(1deg)`;
-    items[i].style.zIndex = -stt;
-    items[i].style.filter = "blur(5px)";
-    items[i].style.opacity = stt > 2 ? 0 : 0.6;
-  }
-}
-loadShow();
-let next = document.getElementById("next");
-let prev = document.getElementById("prev");
-next.onclick = function () {
-  active = active + 1 < items.length ? active + 1 : active;
-  loadShow();
-};
-prev.onclick = function () {
-  active = active - 1 >= 0 ? active - 1 : active;
-  loadShow();
-};
 
-const like = document.querySelector(".like");
+const like = document.querySelector('.like');
 
-let countlike = 0;
-like.addEventListener("click", () => {
-  if (countlike === 0) {
-    like.classList.toggle("anim-like");
-    countlike = 1;
-    like.style.backgroundPosition = "right";
-  } else {
-    countLike = 0;
-    like.style.backgroundPosition = "left";
-  }
+let countLike = 0;
+like.addEventListener('click', () => {
+
+    if(countLike === 0) {
+        like.classList.toggle('anim-like');
+        countLike = 1;
+        like.style.backgroundPosition = 'right';
+    } else {
+        countLike = 0;
+        like.style.backgroundPosition = 'left';
+    }
+
 });
 
-like.addEventListener("animationend", () => {
-  like.classList.toggle("anim-like");
-});
+like.addEventListener('animationend', () => {
+    like.classList.toggle('anim-like');
+})
