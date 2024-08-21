7aece14e888dabef09dcd6db5facd1045ec73200
TITI3048 <164487053+TITI3048@users.noreply.github.com>
2024-08-21 22:07:14 +0200

    connexion.php

---
+++ espace-admin/connexion.php
@@ -0,0 +1,20 @@
+<?php ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Espace de connexion admin</title>
+</head>
+<body>
+    <form method="POST" action="" aligne="center">
+        <input type="text" name="pseudo" autocomplete="off">
+        <br>
+        <input type="password" name="mdp">
+        <br><br>
+        <input type="submit" name="valider">
+    </form>
+</body>
+</html>
