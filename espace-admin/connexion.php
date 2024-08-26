84d8c1aa856a97628523082f0259650d4f78ada5
TITI3048 <164487053+TITI3048@users.noreply.github.com>
2024-08-26 13:47:11 +0200

    test.html test.css

--- espace-admin/connexion.php
+++ espace-admin/connexion.php
@@ -26,7 +26,6 @@ if (isset($_POST['valider'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/espace-admin.css">
     <title>Espace de connexion admin</title>
 </head>
 <body>
