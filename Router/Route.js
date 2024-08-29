0a3d7b430ad964ce3f14cae13d352642632d6119
TITI3048 <164487053+TITI3048@users.noreply.github.com>
2024-08-29 16:27:48 +0200

    Router/allRoutes.js Router/Route.js

---
+++ Router/Route.js
@@ -0,0 +1,8 @@
+export default class route {
+    constructor(url, title, pathHtml, pthJS ="") {
+        this.url = url;
+        this.title = title;
+        this.pathHtml = pathHtml;
+        this.pthJS = pthJS;
+    }
+}
---
+++ Router/allRoutes.js
@@ -0,0 +1,14 @@
+import Route from "./route.js";
+
+export const allRoutes = [
+    new Route("/Accueil", "/Accueil/accueil.html", "/Accueil/accueil.js"),
+    new Route("/connexion, /Connexion/connexion.html", "/Connexion/connexion.js"),
+    new Route("/Services", "/Services/services.html", "/Services/services.js"),
+    new Route("/avis", "/Avis/avis.html", "/Avis/avis.js"),
+    new Route("/jungle", "/Jungle/jungle.html", "/Jungle/jungle.js"),
+    new Route("/reptile", "/Reptile/reptile.html", "/Reptile/reptile.js"),
+    new Route("/reserve africaine", "/ReserveAfricaine/reserveAfricaine.html", "/ReserveAfricaine/reserveAfricaine.js"),
+    new Route("/voliere", "/Voliere/voliere.html", "/Voliere/voliere.js"),
+];
+
+export const websiteName = "ARCADIA ZOO";
