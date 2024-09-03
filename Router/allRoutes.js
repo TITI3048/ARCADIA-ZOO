bffbb103bdce190d3d9832188dba8a48ab75e1ce
TITI3048 <164487053+TITI3048@users.noreply.github.com>
2024-09-03 11:15:59 +0200

    Accueil/accueil.html connexion/connexion.html Router/allRoutes.js

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,8 +1,8 @@
-import Route from "./Route.js";
+import Route from "./router/Route.js";
 
 export const allRoutes = [
     new Route("/Accueil", "/Accueil/accueil.html", "/Accueil/accueil.js"),
-    new Route("/connexion, /Connexion/connexion.html", "/Connexion/connexion.js"),
+    new Route("/connexion, /Connexion/connexion.html", "/Connexion/connexion.js", "/espace-admin/admin.php"),
     new Route("/Services", "/Services/services.html", "/Services/services.js"),
     new Route("/avis", "/Avis/avis.html", "/Avis/avis.js"),
     new Route("/jungle", "/Jungle/jungle.html", "/Jungle/jungle.js"),
