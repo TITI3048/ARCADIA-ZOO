927b798cba026093364e2f82ed49bde1f6d4725b
TITI3048 <164487053+TITI3048@users.noreply.github.com>
2024-09-05 08:33:05 +0200

    correction et modification

--- connexion/connexion.php
+++ connexion/connexion.php
@@ -9,19 +9,19 @@
 </head>
 
 <body>
-    <form>
+    <form method="POST" class="admin">
         <h1>Se Connecter</h1>
         <p class="choose-email"> Accès réservé au personnel</p>
             <div class="mb-3">
-                <label for="EmailInput" class="form-label">adresse email</label> 
-                <input type="email" class="form-control" id="EmailInput" placeholder="joségarcia@mail.fr">
+                <label for="EmailInput" class="form-label" name="pseudo">adresse email</label> 
+                <input type="email" class="form-control" id="EmailInput">
             </div>
             <div class="mb-3">
-                <label for="PassWord" class="form-label">Mot de Passe</label>
-                <input type="password" class="form-control" id="PassWord" placeholder="12340.">
+                <label for="PassWord" class="form-label" name="mdp">Mot de Passe</label>
+                <input type="password" class="form-control" id="PassWord">
             </div>
             <div class="text">
-                <button type="submit" class="btn btn-primary">Connexion
+                <button type="submit" class="btn btn-primary" name="valider">Connexion
                 </button>
                 
             </div>
@@ -37,3 +37,15 @@
 </body>
 
 </html>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administration - Arcadia Zoo</title>
+    <link rel="stylesheet" href="admin.css">
+</head>
+
+<body>
