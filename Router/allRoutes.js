6f721bc56dc6ce1500fc6b84da6d1040cc8123b6
TITI3048 <164487053+TITI3048@users.noreply.github.com>
2024-09-09 16:17:06 +0200

    modif

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,7 +1,7 @@
 import Route from "./Route.js";
 
 export const allRoutes = [
-    /*new Route("/accueil", "/pages/accueil.html", "/js/accueil.js"),
+    new Route("/", "/pages/accueil.html", "/js/accueil.js"),
     new Route("/connexion", "/pages/connexion.html", "/js/connexion.js"),
     new Route("/inscription", "/pages/inscription.html", "/js/inscription.js"),
     new Route("/services", "/pages/services.html"),
@@ -9,7 +9,7 @@ export const allRoutes = [
     new Route("/jungle", "/pages/jungle.html" , "/js/jungle.js"),
     new Route("/reptile", "/pages/reptile.html", "/js/reptile.js"),
     new Route("/reserve-africaine", "/pages/reserve-africaine.html", "/js/reserve-africaine.js"),
-    new Route("/voliere", "/pages/voliere.html", "/js/voliere.js"),*/
+    new Route("/voliere", "/pages/voliere.html", "/js/voliere.js"),
     new Route("/", "/pages/ok.html")
 ];
 
