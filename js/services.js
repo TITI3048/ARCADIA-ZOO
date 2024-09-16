d4a76db38047b1141bc11062464a07c12005aa87
TITI3048 <thibaud_rouviere@yahoo.fr>
2024-09-16 18:14:25 +0200

    modification responsive

--- js/services.js
+++
@@ -1,7 +0,0 @@
-var button = document.querySelector('.button');
-        var menu = document.querySelector('.menu');
-
-        button.onclick = function() {
-            button.classList.toggle('active');
-            menu.classList.toggle('active');
-}
