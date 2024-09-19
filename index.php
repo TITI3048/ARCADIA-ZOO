3c8bf9a99df936cac8c14cced4e95c5bac5bdfff
TITI3048 <thibaud_rouviere@yahoo.fr>
2024-09-19 13:47:53 +0200

    modif

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/accueil.css" />
+    <title>Document</title>
+</head>
+
+<body>
+    <header>zoo
+        <img src="/image/LOGO RINO 3.png" class="image">
+        <div class="button"></div>
+        <ul class="menu">
+            <li><a href="services.html">Services</a></li>
+            <li><a href="jungle.html">La Jungle</a></li>
+            <li><a href="reptile.html">Les Réptiles</a></li>
+            <li><a href="reserve-africaine.html">La Réserve Africaine</a></li>
+            <li><a href="voliere.html">Les Oiseaux</a></li>
+            <li><a href="connexion.php">Connexion</a></li>
+        </ul>
+    </header>
+    
+    <footer>
+        <p class="contener">&copy; 2024 Arcadia Zoo Forêt de Broceliande</p>
+        <img src="/image/image footer/icon-facebook.png" class="logo" alt="facebook">
+        <img src="/image/image footer/icon-instagram.png" class="logo" alt="instagram">
+        <img src="/image/image footer/icon-x.png" class="logo" alt="x">
+        <img src="/image/image footer/icon-whatsapp.jpg" class="logo" alt="whatsapp">
+    </footer>
+</body>
+
+</html>
