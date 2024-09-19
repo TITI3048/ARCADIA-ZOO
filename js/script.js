7e87b1c79172fa726320945e6893a3b708b6e75e
TITI3048 <thibaud_rouviere@yahoo.fr>
2024-09-19 13:40:55 +0200

    modification

--- js/script.js
+++ js/script.js
@@ -1,49 +1,32 @@
 const tokenCookieName = "accesstoken";
 
-function setToken(token){
+function setToken(token) {
     setCookie(tokenCookieName, token, 1);
 }
 
-function getToken(){
+function getToken() {
     return getCookie(tokenCookieName);
 }
 
-function setCookie(name,value,day) {
-    var expire ="";
+function setCookie(name, value, days) {
+    var expires = "";
     if (days) {
         var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         expires = "; expires=" + date.toUTCString();
     }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
 
 function getCookie(name) {
     var nameEQ = name + "=";
-    var ca = document.cookie.split(";");
+    var ca = document.cookie.split(';');
     for (const c of ca) {
         let cookie = c;
-        while (cookie.startsWith(" ")) cookie = cookie.substring(1, cookie.length);
+        while (cookie.startsWith(' ')) cookie = cookie.substring(1, cookie.length);
         if (cookie.startsWith(nameEQ)) return cookie.substring(nameEQ.length, cookie.length);
     }
     return null;
-}
-
-function eraseCookie(name) {
-    document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-}
-
-function isConnected(){
-    if(getToken() == null || getToken == undefined){
-        return false;
-    }
-    else{
-        return true;
-    }
-}
-
-if(isConnected()){
-    alert("je suis connecté");
 }
 
 var button = document.querySelector('.button');
